2d043c521764cd8fd605fc822547c14852ed34e8
Shania Muller <shaniamuller@hotmail.com>
2025-02-01 16:12:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java17LinkedInDemo
+ Demo code that follows the LinkedIn Learning - Learning Java 17 by Kathryn Hodge video.
---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Beginner Hello World class.
+ * Followed LinkedIn Learning Video - Learning Java 17 by Kathryn Hodge
+ *     ~ https://www.linkedin.com/learning/learning-java-17 ~
+ * Started Feb 2025
+ *
+ * @author Shania Muller
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! 'Tis I, Shania");
+    }
+}
