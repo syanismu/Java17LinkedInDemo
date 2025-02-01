c7ac06550f29cec2589c88244e84235ccbc64ec7
Shania Muller <shaniamuller@hotmail.com>
2025-02-01 17:06:48 -0500

    HelloWorld v1.1
    Added comments and extra print statements to show the difference between:
    System.out.println
    and
    System.out.print
    
    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Java17LinkedInDemo
- Demo code that follows the LinkedIn Learning - Learning Java 17 by Kathryn Hodge video.
+# Java17 LinkedIn Demo
+Demo code that follows the LinkedIn Learning video - [Learning Java 17 by Kathryn Hodge](https://www.linkedin.com/learning/learning-java-17)
+ 
+Created By: Shania Muller
+
+Started: Feb 2025
+
+## Objective
+This demo is to refresh my memory on the fundamentals of Java Programming and to showcase my skills for potential employers.
+
+If you are reading this, welcome :) 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,10 +5,20 @@
  * Started Feb 2025
  *
  * @author Shania Muller
- * @version 1.0
+ * @version 1.1
  */
+
 public class HelloWorld {
+    // psvm - main function, entry point for code
     public static void main(String[] args) {
-        System.out.println("Hello World! 'Tis I, Shania");
+        // Print to the console
+        System.out.println("Hello World! 'Tis I, Shania. ");
+
+        // PRINT vs PRINTLN
+        // print - does NOT have a newline \n character appended to the end
+        System.out.print("This text does not contain a newline. ");
+        // println - DOES have \n at the end
+        System.out.println("As you can see, this is on the same line as the last sentence. ");
+        System.out.println("New Line :)");
     }
 }
