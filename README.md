fc7e50a9a55a2a2f05cca3f889b11dff0510cfd8
Shania Muller <shaniamuller@hotmail.com>
2025-02-01 20:44:22 -0500

    HelloWorld V1.2
    Finished section 2. Building Strong Foundations.
    
    This focused on Primitive Datatypes like int, double, char and boolean as well as Reference types such as Strings.  Concatenation and charAt were used as well.

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Started: Feb 2025
 
 ## Objective
 This demo is to refresh my memory on the fundamentals of Java Programming and to showcase my skills for potential employers.
+Personal comments will be left to explain the code and my understanding.
 
 If you are reading this, welcome :) 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Beginner Hello World class.
  * Followed LinkedIn Learning Video - Learning Java 17 by Kathryn Hodge
@@ -5,7 +7,7 @@
  * Started Feb 2025
  *
  * @author Shania Muller
- * @version 1.1
+ * @version 1.2
  */
 
 public class HelloWorld {
@@ -20,5 +22,51 @@ public class HelloWorld {
         // println - DOES have \n at the end
         System.out.println("As you can see, this is on the same line as the last sentence. ");
         System.out.println("New Line :)");
+
+        // DATATYPES and VARIABLES
+        // Data type is integer - whole number, variable name is studentAge, value is initialized to 15
+        int studentAge = 27;
+        // double - decimal number
+        double studentGPA = 86.17;
+        // char - single character
+        // single quotes used for char characters
+        char studentFirstInitial = 'S';
+        char studentLastInitial ='M';
+        // boolean - true or false
+        boolean hasPerfectAttendance = true;
+
+        //STRINGS
+        // String type needs to start with a capital S
+        String studentFirstName = "Shania";
+        String studentLastName =  "Muller";
+
+        // Calculate the first initial using studentName and charAt
+        // charAt returns the char at the requested index
+        char studentFirstInitialCalculated = studentFirstName.charAt(0);
+        char studentLastInitialCalculated = studentLastName.charAt(0);
+
+        // Prints 'Shania Muller' to the console
+        System.out.println(studentFirstName + " " + studentLastName);
+        // Prints '27' to the console
+        System.out.println(studentAge);
+        // Prints 'S' to the console
+        System.out.println(studentFirstInitial);
+        // Prints 'M' to the console
+        System.out.println(studentLastInitial);
+        // Prints 'S' to the console
+        System.out.println(studentFirstInitialCalculated);
+        // Prints 'M' to the console
+        System.out.println(studentLastInitialCalculated);
+
+        // CONCATENATION - combining 2+ variables or strings into a single string
+        System.out.println(studentFirstName + " " + studentLastName + "'s GPA is " + studentGPA);
+        // Asking user for input
+        System.out.println("What would you like to update it to? ");
+        // Gather user input using Scanner
+        Scanner scanner = new Scanner(System.in);
+        // Overwrite the old value with a new one
+        studentGPA = scanner.nextDouble();
+        // Print the new value to the console
+        System.out.println(studentFirstName + " " + studentLastName + "'s GPA is now " + studentGPA);
     }
 }
