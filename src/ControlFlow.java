d94dd692dcc623d73f42a7d282910c01be6be756
Shania Muller <shaniamuller@hotmail.com>
2025-02-04 14:23:09 -0500

    Control Flow V1.1 - Scope Example
    Added on to the previous example to showcase how scope works.

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -7,11 +7,18 @@ import java.util.Scanner;
  * Started Feb 2025
  *
  * @author Shania Muller
- * @version 1.0
+ * @version 1.1
  */
 
 public class ControlFlow {
     public static void main(String[] args) {
+        // SCOPE - The area in which a variable is accessible
+        // This variable's scope is local to psvm
+        String shaniasWord = "Katzen";
+        // printf - print format, allows you to format a string using specifiers
+        System.out.printf("This word is accessible in all of PSVM: %s\n", shaniasWord);
+
+        // USER INPUT
         // Ask the user to input a number into the console
         System.out.println("~ Pick a number between 1 and 10 ~ ");
         // Declare and instantiate a Scanner object
@@ -22,11 +29,17 @@ public class ControlFlow {
         // Control Flow - If Statement
         // Determine whether the chosen number is greater or less than 5
         if (chosenNum < 5) {
-            // If less than 5, print tomato
-            System.out.println("Tomato");
+            // If less than 5, print new word
+            shaniasWord = "Koyangi";
+            System.out.printf("You picked %d! The word is now: %s\n", chosenNum, shaniasWord);
         } else {
-            // if more than 5, print potato
-            System.out.println("Potato");
+            // if more than 5, print new word
+            shaniasWord = "Neko";
+            System.out.printf("You picked %d! The word is now: %s\n", chosenNum, shaniasWord);
         }
+
+        // Despite shaniasWord changing in the if statement, the value remains the same due to the scope
+        System.out.printf("And yet, outside of the scope of the if statement, the word remains: %s\n", shaniasWord);
+
     }
 }
